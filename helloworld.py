6cbeea1fdc65e7618a2bdc05bd5a4b2be1acfbc7
Patrick Dolloso <patrick.dolloso@gmail.com>
2018-09-27 05:55:38 -0400

    commit

---
+++ helloworld.py
@@ -0,0 +1,38 @@
+# Hello World Patrick Portfolio
+import time
+a = 'hello world!'
+b = 'My name is Patrick Dolloso.'
+c = 'I recently graduated in May 2018 with my Dual Bachelors '
+c1 = 'in Engineering & Management (specialization in nuclear engineering) ' 
+c2 = 'from the University of Ontario Institute of Technology in Canada.'
+
+d = 'I am a lifelong learner'
+e = 'I have a passion in STEM and I love to challenge myself.'
+
+f = 'Please email me at: '
+f1 = 'patrick.dolloso@gmail.com for all inquiries.'
+g = 'I hope you will enjoy taking a look at my projects.'
+h = 'have a wonderful day! :D'
+
+print(a)
+time.sleep(2)
+print(b)
+time.sleep(2)
+print(c)
+time.sleep(1)
+print(c1)
+time.sleep(1)
+print(c2)
+time.sleep(2)
+print(d)
+time.sleep(2)
+print(e)
+time.sleep(2)
+print(f)
+time.sleep(1)
+print(f1)
+time.sleep(2)
+print(g)
+time.sleep(2)
+print(h)
+
